d25fe36927894784958dbdd4f092c248feac5121
Sébastien Sénéchal <sebast.senechal@gmail.com>
2019-11-01 12:33:13 +0100

    CSS style adjustments

--- view/nav.php
+++ view/nav.php
@@ -1,7 +1,6 @@
 <nav id="main_navigation">
       <ul>
         <li><a href="index.php">Accueil</a></li>
-        <li><a href="">Projets</a></li>
         <li><a href="">Contact</a></li>
         <li><a href="index.php?action=login">Connexion / Inscription</a></li> <!-- action=login appel la fonction "login() du controleur" -->
       </ul>
