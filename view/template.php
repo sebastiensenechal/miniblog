47cc35dacec20694d2167be19f44be1bc26f9e19
Sébastien Sénéchal <sebast.senechal@gmail.com>
2019-11-28 15:24:56 +0100

    Adjust meta in template and style css

--- view/template.php
+++ view/template.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html lang="fr">
   <head>
+    <title>Jean Forteroche | <?= htmlspecialchars($title) ?></title>
     <meta charset="UTF-8">
-    <link href="./public/css/style.css" rel="stylesheet">
     <meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>Jean Forteroche | <?= htmlspecialchars($title) ?></title>
+    <meta name="description" content="Billet simple pour l'Alaska, un roman de Jean Forteroche."/>
+    <meta name="author" content="Jean Forteroche">
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="Jean Forteroche" />
+    <meta name="twitter:creator" content="Sébastien Sénéchal" />
+    <meta property="og:url" content="https://sebastiensenechal.com/openclassroom/miniblog/" />
+    <meta property="og:title" content="Jean Forteroche" />
+    <meta property="og:description" content="Billet simple pour l'Alaska, un roman de Jean Forteroche." />
+    <meta property="og:image" content="https://sebastiensenechal.com/openclassroom/miniblog/public/img/montagnes-pyrenees-480.jpg" />
+    <meta property="og:title" content="Jean Forteroche" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://sebastiensenechal.com/openclassroom/miniblog/" />
+    <meta property="og:image" content="https://sebastiensenechal.com/openclassroom/miniblog/public/img/montagnes-pyrenees-480.jpg" />
+    <link href="./public/css/style.css" rel="stylesheet">
+    <link rel="icon" type="image/ico" href="./public/img/favicon.ico" />
   </head>
   <body>
 
